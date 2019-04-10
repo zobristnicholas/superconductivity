4ccb2c922aa7e04e7cf7cd1c0e97095dc1c413f7
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-04-10 12:41:07 -0700

    complex conductivity limit properly broadcasts input arrays and quad uses temperature dependent gap energy

--- superconductivity/complex_conductivity.py
+++ superconductivity/complex_conductivity.py
@@ -1,11 +1,17 @@
+import numba
 import numpy as np
 import scipy.special as sp
 import scipy.constants as sc
+import scipy.integrate as it
+
+from superconductivity.fermi_functions import fermi
+from superconductivity.gap_functions import deltar_bcs
 
 
 def limit(temp, freq, delta1, delta2=0.):
-    """Calculates the approximate complex conductivity in the limit hf << ∆ and
-    kB T << ∆ given some temperature, frequency and transition temperature.
+    """Calculate the approximate complex conductivity to normal conductivity
+    ratio in the limit hf << ∆ and kB T << ∆ given some temperature, frequency
+    and transition temperature.
     Parameters
     ----------
     temp : float, numpy.ndarray
@@ -21,19 +27,25 @@ def limit(temp, freq, delta1, delta2=0.):
     -------
     sigma : numpy.ndarray, dtype=numpy.complex128
         The complex conductivity at temp and freq."""
-    assert np.all(temp > 0), "Temperature must be >= 0."
-    size_t = np.size(temp)
-    size_f = np.size(f)
-    assert size_t == size_f or size_t == 0 or size_f == 0, "Incompatible array sizes"
+    temp = np.atleast_1d(temp)
+    freq = np.atleast_1d(freq)
 
-    sigma1 = np.zeros(max(size_t, size_f))
-    sigma2 = np.zeros(max(size_t, size_f))
+    assert (temp > 0).all(), "Temperature must be >= 0."
+    if temp.size == 1 and freq.size != 1:
+        temp = np.ones(freq.size) * temp
+    elif freq.size == 1 and temp.size != 1:
+        freq = np.ones(temp.size) * freq
+    elif freq.size != temp.size:
+        raise ValueError("Incompatible array sizes")
 
-    xi = sc.h * freq / (2 * sc.k * temp)
-    eta = delta1 / (sc.k * temp)
+    sigma1 = np.zeros(freq.size)
+    sigma2 = np.zeros(freq.size)
 
-    sigma1[temp == 0] = np.pi * delta2 / (sc.h * freq) + 1j * np.pi * delta1 / (sc.h * freq)
-    sigma2[temp == 0] = np.pi * delta1 / (sc.h * freq)
+    xi = sc.h * freq / (2 * sc.k * temp[temp != 0])
+    eta = delta1 / (sc.k * temp[temp != 0])
+
+    sigma1[temp == 0] = np.pi * delta2 / (sc.h * freq[temp == 0])
+    sigma2[temp == 0] = np.pi * delta1 / (sc.h * freq[temp == 0])
     sigma1[temp != 0] = (4 * delta1 / (sc.h * freq) * np.exp(-eta) * np.sinh(xi) * sp.k0(xi) +
                          np.pi * delta2 / (sc.h * freq) *
                          (1 + 2 * delta1 / (sc.k * temp) * np.exp(-eta) * np.exp(-xi) * sp.i0(xi)))
@@ -42,14 +54,15 @@ def limit(temp, freq, delta1, delta2=0.):
     return sigma1 + 1j * sigma2
 
 
-def quadrature(temp, freq, delta1):
-    """Calculates the approximate complex conductivity by integrating given
-    some temperature, frequency and transition temperature.
+def quadrature(temp, freq, delta1, bcs=1.764):
+    """Calculate the approximate complex conductivity to normal conductivity
+    ratio by integrating given some temperature, frequency and transition temperature,
+    where hf < ∆ (tones with frequency, f, do not break Cooper pairs).
     Parameters
     ----------
-    temp : float, numpy.ndarray
+    temp : iterable of size N
         Temperature in units of Kelvin.
-    freq : float, numpy.ndarray
+    freq : float
         Frequency in units of Hz.
     delta1: float
         Superconducting gap energy at 0 Kelvin in units of Joules.
@@ -57,4 +70,67 @@ def quadrature(temp, freq, delta1):
     -------
     sigma : numpy.ndarray, dtype=numpy.complex128
         The complex conductivity at temp and freq."""
-    raise NotImplementedError
+    # coerce inputs into numpy array
+    temp = np.atleast_1d(temp)
+    # get the temperature dependent gap
+    delta = delta1 * deltar_bcs(bcs * sc.k * temp / delta1, bcs=bcs)
+    # calculate unitless reduced temperature and frequency
+    t = temp * sc.k / delta
+    w = sc.h * freq / delta
+    # set the temperature independent bounds for integrals
+    a1, b1, b2 = 1, np.inf, np.pi / 2
+    # allocate memory for arrays
+    sigma1 = np.zeros(temp.size)
+    sigma2 = np.zeros(temp.size)
+    # compute the integral by looping over inputs
+    for ii in range(temp.size):
+        a2 = np.arcsin(1 - w[ii])
+        sigma1[ii] = it.quad(sigma1_kernel, a1, b1, args=(t[ii], w[ii]))[0]
+        sigma2[ii] = it.quad(sigma2_kernel, a2, b2, args=(t[ii], w[ii]))[0]
+
+    return sigma1 + 1j * sigma2
+
+
+def sigma1_kernel(e, t, w):
+    """Calculate the kernel of the integral for the real part of the complex
+    conductivity where hf < ∆ (tones with frequency, f, do not break Cooper pairs).
+    Parameters
+    ----------
+    e: numpy.ndarray
+        reduced energy (E / ∆)
+    t: float
+        reduced temperature (kB T / ∆)
+    w: float
+        reduced frequency (h f / ∆)
+    Returns
+    -------
+    k: numpy.ndarray
+        The kernel for the integral for the real part of the complex conductivity
+    """
+    k = (2 * (fermi(e, t) - fermi(e + w, t)) * (e**2 + w * e + 1) /
+             (w * np.sqrt(e**2 - 1) * np.sqrt((e + w)**2 - 1)))
+    return k
+
+
+def sigma2_kernel(y, t, w):
+    """Calculate the kernel of the integral for the imaginary part of the complex
+    conductivity where hf < ∆ (tones with frequency, f, do not break Cooper pairs)
+    for arcsin(1 - w) < y < pi / 2. Using e = sin(y) substitution in the
+    dimensionless integral.
+    Parameters
+    ----------
+    y: numpy.ndarray
+        e = sin(y) where e is the reduced energy (E / ∆)
+    t: float
+        reduced temperature (kB T / ∆)
+    w: float
+        reduced frequency (h f / ∆)
+    Returns
+    -------
+    k: numpy.ndarray
+        The kernel for the integral for the imaginary part of the complex conductivity
+    """
+    k = ((1 - 2 * fermi(np.sin(y) + w, t)) * (np.sin(y)**2 + w * np.sin(y) + 1) /
+         (w * np.sqrt((np.sin(y) + w)**2 - 1)))
+    return k
+
