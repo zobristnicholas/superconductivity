9ff14447cbd557f0cb13a36e967002235342cbcd
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-06-05 17:30:44 -0700

    Updated and refactored bcs density of states function

--- superconductivity/density_of_states.py
+++ superconductivity/density_of_states.py
@@ -1,7 +1,8 @@
 import numpy as np
+from numpy.lib.scimath import sqrt  # sqrt that doesn't error on negative floats
 
 
-def model(en, delta, gamma=0, amplitude=0, ep=0, fwhm=0):
+def dos_bcs(en, delta, gamma=0, amplitude=0, ep=0, fwhm=0, abs_real=True, norm=False):
     """
     Compute the density of states for a generic BCS superconductor with the
     option of a small Lorentzian distribution of inter-gap states. Functional
@@ -26,13 +27,52 @@ def model(en, delta, gamma=0, amplitude=0, ep=0, fwhm=0):
     fwhm: float (optional)
         Full-width-half-max energy in units of en for the Lorentzian
         distribution. Cannot be zero if amplitude is not zero.
+    abs_real: boolean (optional)
+        If False, the absolute value of the real part of the result is not
+        taken. This would not return the true density of states but is useful
+        for computing integrals where -delta < en < delta. The default is True.
+    norm: boolean (optional)
+        If True, normalize the density of states by the energy. This is useful
+        if multiplying by a function that contains 1 / energy to avoid
+        singularities. If en = 0, nothing is done. The default is False.
     Returns
     -------
-    dos: float, numpy.ndarray
+    dos: numpy.ndarray
         density of states as a function of en
     """
-    assert not (amplitude != 0 and fwhm == 0), "If the amplitude is not zero, fwhm must not be zero"
-    dos = np.abs(np.real((en - 1j * gamma) / np.sqrt((en - 1j * gamma)**2 - delta**2)))
-    if amplitude != 0 and fwhm != 0:
-        dos += 2 * amplitude * fwhm / np.pi / (4 * (en - ep)**2 + fwhm**2)
+    en = np.atleast_1d(en)
+    dos = np.empty(en.shape, dtype=np.complex)
+    # determine density of states
+    if norm:
+        logic = (en != 0)
+        dos[logic] = (en[logic] - 1j * gamma) / sqrt((en[logic] - 1j * gamma)**2 - delta**2) / en[logic]
+        logic = np.logical_not(logic)
+        dos[logic] = (-1j * gamma) / sqrt((-1j * gamma)**2 - delta**2)
+    else:
+        dos = (en - 1j * gamma) / sqrt((en - 1j * gamma)**2 - delta**2)
+    # get real value or fix branch cut
+    if abs_real:
+        dos = np.abs(np.real(dos))
+    else:
+        # make sure that the branch cut on sqrt doesn't mess things up
+        if norm:
+            logic = (en < 0) & (dos.imag > 0)
+            dos[logic] = np.conj(dos[logic])
+        else:
+            logic = (en < 0) & (dos.imag < 0)
+            dos[logic] = np.conj(dos[logic])
+        logic = (en > 0) & (dos.imag > 0)
+        dos[logic] = np.conj(dos[logic])
+    # add an optional lorentzian distribution
+    if amplitude != 0:
+        if fwhm != 0:
+            if norm:
+                logic = (en != 0)
+                dos[logic] += 2 * amplitude * fwhm / np.pi / (4 * (en[logic] - ep) ** 2 + fwhm ** 2) / en[logic]
+                logic = np.logical_not(logic)
+                dos[logic] += 2 * amplitude * fwhm / np.pi / (4 * (en[logic] - ep) ** 2 + fwhm ** 2)
+            else:
+                dos += 2 * amplitude * fwhm / np.pi / (4 * (en - ep)**2 + fwhm**2)
+        else:
+            raise ValueError("If the amplitude is not zero, fwhm must not be zero")
     return dos
