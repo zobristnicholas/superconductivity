77e615c95e774c1077b12eaf40593a3e03247911
Nicholas Zobrist <nzobrist@physics.ucsb.edu>
2019-06-19 11:50:59 -0700

    density of states and density of pairs now return imaginary part only if real=False

--- superconductivity/density_of_pairs.py
+++ superconductivity/density_of_pairs.py
@@ -13,8 +13,10 @@ def dop_bcs(en, delta, real=True):
     delta: float
         Superconducting gap energy in units of en.
     real: boolean (optional)
-        If False, the complex valued function is returned. If True, the real
-        valued density of states function is returned. The default is True.
+        If False, the imaginary part of the complex valued function is
+        returned. If True, the real part of the complex valued function
+        is returned. The real part is the density of pairs. The default is
+        True.
     Returns
     -------
     dop: numpy.ndarray
@@ -27,13 +29,13 @@ def dop_bcs(en, delta, real=True):
     if not real:  # imaginary part has the wrong sign for energies less than zero
         logic = (en < 0)
         dop[logic] = np.conj(dop[logic])
-    return dop.real if real else dop
+    return dop.real if real else dop.imag
 
 
 def dop_dynes(en, delta, gamma, real=True):
     """
     Compute the density of pairs for a Dynes superconductor. Functional
-    form from Herman T. et al. Phys. Rev. B, 96, 1, 2017.
+    form from Herman et al. Phys. Rev. B, 96, 1, 2017.
     Parameters
     ----------
     en: float, numpy.ndarray
@@ -43,8 +45,10 @@ def dop_dynes(en, delta, gamma, real=True):
     gamma: float (optional)
         Dynes parameter for broadening the density of states in units of en.
     real: boolean (optional)
-        If False, the complex valued function is returned. If True, the real
-        valued density of states function is returned. The default is True.
+        If False, the imaginary part of the complex valued function is
+        returned. If True, the real part of the complex valued function
+        is returned. The real part is the density of pairs. The default is
+        True.
     Returns
     -------
     dop: numpy.ndarray
@@ -54,4 +58,4 @@ def dop_dynes(en, delta, gamma, real=True):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", RuntimeWarning)  # divide by zero
         dop = np.sign(en) * delta / sqrt((en + 1j * gamma)**2 - delta**2)
-    return dop.real if real else dop
+    return dop.real if real else dop.imag
--- superconductivity/density_of_states.py
+++ superconductivity/density_of_states.py
@@ -13,8 +13,10 @@ def dos_bcs(en, delta, real=True):
     delta: float
         Superconducting gap energy in units of en.
     real: boolean (optional)
-        If False, the complex valued function is returned. If True, the real
-        valued density of states function is returned. The default is True.
+        If False, the imaginary part of the complex valued function is
+        returned. If True, the real part of the complex valued function
+        is returned. The real part is the density of states. The default is
+        True.
     Returns
     -------
     dos: numpy.ndarray
@@ -27,13 +29,13 @@ def dos_bcs(en, delta, real=True):
     if not real:  # imaginary part has the wrong sign for energies less than zero
         logic = (en < 0)
         dos[logic] = np.conj(dos[logic])
-    return dos.real if real else dos
+    return dos.real if real else dos.imag
 
 
 def dos_dynes(en, delta, gamma, real=True):
     """
     Compute the density of states for a Dynes superconductor. Functional
-    form from Herman T. et al. Phys. Rev. B, 96, 1, 2017.
+    form from Herman et al. Phys. Rev. B, 96, 1, 2017.
     Parameters
     ----------
     en: float, numpy.ndarray
@@ -43,8 +45,10 @@ def dos_dynes(en, delta, gamma, real=True):
     gamma: float (optional)
         Dynes parameter for broadening the density of states in units of en.
     real: boolean (optional)
-        If False, the complex valued function is returned. If True, the real
-        valued density of states function is returned. The default is True.
+        If False, the imaginary part of the complex valued function is
+        returned. If True, the real part of the complex valued function
+        is returned. The real part is the density of states. The default is
+        True.
     Returns
     -------
     dos: numpy.ndarray
@@ -54,4 +58,4 @@ def dos_dynes(en, delta, gamma, real=True):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", RuntimeWarning)  # divide by zero
         dos = np.sign(en) * (en + 1j * gamma) / sqrt((en + 1j * gamma)**2 - delta**2)
-    return dos.real if real else dos
+    return dos.real if real else dos.imag
